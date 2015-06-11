139f9f7cc6bbbd7f3e57311120c9e33422d572d3
adamdehaven <adamdehaven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-11 17:53:08 +0000

    Resolving errors and upping version to 1.2.5
    git-svn-id: https://plugins.svn.wordpress.org/perfect-pullquotes/trunk@1179151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- perfect-pullquotes.js
+++ perfect-pullquotes.js
@@ -1,6 +1,6 @@
 /* ======================================
  * https://github.com/adamdehaven/perfect-pullquotes
- * version 1.2.4
+ * version 1.2.5
  *
  * Adam Dehaven ( @adamdehaven )
  * http://adamdehaven.com/
